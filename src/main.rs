8c8e976f067aace5f6afaa3ad0180c1602aebfea
ethanhunsaker <ethan.m.hunsaker@gmail.com>
2025-02-28 22:35:20 -0700

    feat: add camera

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::io;
 use std::ops::Mul;
 use crate::color::Color;
 use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
 
 fn ray_color(r: &Ray) -> Color {
     let unit_direction = vec3::unit_vector(r.direction());
@@ -17,6 +18,17 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal/2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
