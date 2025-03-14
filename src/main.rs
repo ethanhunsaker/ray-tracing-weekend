1965113d5326a7259463e1e5b499a125de70ca6b
ethanhunsaker <ethan.m.hunsaker@gmail.com>
2025-03-13 22:35:12 -0600

    feat: add normals

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,24 @@ use crate::vec3::{Point3, Vec3};
 use std::io;
 use std::ops::Mul;
 
-fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
     let a = vec3::dot(r.direction(), r.direction());
     let b = 2.0 * vec3::dot(oc, r.direction());
     let c = vec3::dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant >= 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - f64::sqrt(discriminant)) / (2.0 * a)
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
     let unit_direction = vec3::unit_vector(r.direction());
@@ -46,7 +52,7 @@ fn main() {
 
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    for j in 0..IMAGE_HEIGHT {
+    for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {}", (IMAGE_HEIGHT - j));
         for i in 0..IMAGE_WIDTH {
             let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
